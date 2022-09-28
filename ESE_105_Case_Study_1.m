743a206e81d52acbda9ddba0c4e9d2c358cc458b
FireSZNLarry <112635784+FireSZNLarry@users.noreply.github.com>
2022-09-28 17:17:12 -0500

    Add files via upload

--- ESE_105_Case_Study_1.m
+++ ESE_105_Case_Study_1.m
@@ -1,10 +1,14 @@
 load COVIDbyCounty.mat
 
- index = find(table2array(COVID_allUS(:,'date'))>="2020-03-18");
- COVID_data = COVID_allUS(index,:);
- COVID_STLcity = table2array(COVID_data(:,'fips'))== 29510;
- COVID_STLcity  = COVID_data(COVID_STLcity,:);
- COVID_STLcnty = table2array(COVID_data(:,'fips'))== 29189;
- COVID_STLcnty = COVID_data(COVID_STLcnty,:);
- COVID_STLmetro = table2array(COVID_data(:,'fips'))== 29183 & 29099 & 29071 & 29219 & 29113 & 17133 & 17119 & 17163 & 17083 & 17013 & 17117 & 17005 & 17027;
- COVID_STLmetro = COVID_data(COVID_STLmetro,:);
+ %index = find(table2array(dates(:,'date'))>="2020-04-02");
+ %dates = dates(index,:);
+ %COVID_STLcity = table2array(CNTY_CENSUS(:,'fips'))== 29510;
+ %COVID_STLcity  = CNTY_CENSUS(COVID_STLcity,:);
+ %COVID_STLcnty = table2array(CNTY_CENSUS(:,'fips'))== 29189;
+ %COVID_STLcnty = CNTY_CENSUS(COVID_STLcnty,:);
+ %COVID_STLmetro = table2array(CNTY_CENSUS(:,'fips'))== 29183 & 29099 & 29071 & 29219 & 29113 & 17133 & 17119 & 17163 & 17083 & 17013 & 17117 & 17005 & 17027;
+ %COVID_STLmetro = CNTY_CENSUS(COVID_STLmetro,:);
+
+ index = find(table2array(CNTY_CENSUS(:,'fips'))>= 29510);
+ COVID_data = CNTY_CENSUS(index,:);
+display(COVID_data);
